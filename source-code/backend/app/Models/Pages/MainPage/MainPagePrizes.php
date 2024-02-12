c538994956b04eb728ef48d247786dfab185bc18
Ruslan <rik.chanel@gmail.com>
2024-02-12 15:19:55 +0300

    add service for image route download

--- source-code/backend/app/Models/Pages/MainPage/MainPagePrizes.php
+++ source-code/backend/app/Models/Pages/MainPage/MainPagePrizes.php
@@ -2,6 +2,7 @@
 
 namespace App\Models\Pages\MainPage;
 
+use App\Services\Helpers\Images\ImageHelperService;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -12,4 +13,22 @@ class MainPagePrizes extends Model
     protected $table = 'main_page_blocks_prizes';
 
     protected $fillable = ['image', 'text'];
+
+    private ImageHelperService $imageHelper;
+
+    public function __construct()
+    {
+        $this->imageHelper = app(ImageHelperService::class);
+        $this->imageHelper->setSavingPath('main-page/images');
+    }
+
+    public function setImageAttribute($value)
+    {
+        $this->attributes['image'] = $this->imageHelper->handleImageUpload(
+            value: $value,
+            model: $this,
+            attribute: 'image'
+        );
+        $this->save();
+    }
 }
