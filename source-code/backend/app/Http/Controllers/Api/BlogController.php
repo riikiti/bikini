3d5c30686f8c9b76ec9da6d0f237e7deced3f02c
mlevsha <mlevsha@team.amocrm.com>
2024-02-21 17:14:35 +0300

    added check for purchase/subscription to box/blog

--- source-code/backend/app/Http/Controllers/Api/BlogController.php
+++ source-code/backend/app/Http/Controllers/Api/BlogController.php
@@ -5,19 +5,39 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\Blog\BlogResource;
 use App\Models\Blog;
+use App\Models\BlogUsers;
 use App\Models\User;
+use Illuminate\Http\Request;
+use Carbon\Carbon;
 
 class BlogController extends Controller
 {
-    public function show($id)
+    public function show(Request $request, $id)
     {
-        $blog = Blog::query()->where('id', $id)->first();
 
-        if (!empty($blog)) {
-            return response()->json(['status' => 'success', 'data' => BlogResource::make($blog)]);
+        $user = $request->user();
+
+        $checkDate = Carbon::now()->addMonths(1);
+
+        $userBlog = BlogUsers::query()
+            ->where('user_id', $user->id)
+            ->where('blog_id', $id)
+            ->where('created_at','>',Carbon::now())
+            ->where('created_at','<', $checkDate)
+            ->exists();
+
+        if ($userBlog) {
+            $blog = Blog::query()->where('id', $id)->first();
+
+            if (!empty($blog)) {
+                return response()->json(['status' => 'success', 'data' => BlogResource::make($blog)]);
+            } else {
+                return response()->json(['status' => 'not found']);
+            }
         } else {
-            return response()->json(['status' => 'not found']);
+            return response()->json(['status' => 'permission denied']);
         }
+
     }
 
     public function getAll(User $user)
