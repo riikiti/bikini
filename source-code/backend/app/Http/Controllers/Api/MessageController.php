3e030da93e7085fbb22f3c91ae33e2bdf2cff334
mlevsha <mlevsha@team.amocrm.com>
2024-03-04 13:27:01 +0300

    message correct work

--- source-code/backend/app/Http/Controllers/Api/MessageController.php
+++ source-code/backend/app/Http/Controllers/Api/MessageController.php
@@ -3,6 +3,7 @@
 namespace app\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\UserCompactResource;
 use App\Models\Message;
 use App\Models\User;
 use Illuminate\Http\JsonResponse;
@@ -12,8 +13,6 @@ class MessageController extends Controller
 {
     public function index(Request $request, User $user): JsonResponse
     {
-        // короче тут не правильно работает, тут я должен получать сообщения из конкретного чата
-        //а получается так что, получаю все собщение двух юзеров
         $check = $this->checkService->checkUser($request);
 
         if ($check) {
@@ -24,11 +23,9 @@ class MessageController extends Controller
         } else {
             // Получаем текущего пользователя
             $senderUser = $request->user();
-
             // Получаем все сообщения, связанные с этим пользователем
-            $receivedMessages = $senderUser->receivedMessages;
-            $sentMessages = $senderUser->sentMessages;
-
+            $receivedMessages = $senderUser->receivedMessages->where('sender_id',$user->id);
+            $sentMessages = $senderUser->sentMessages->where('receiver_id',$user->id);
             return response()->json(['status' => 'success', 'data' =>
                 ['messages' => [
                     'receiver_user' => $user->options(),
@@ -40,10 +37,9 @@ class MessageController extends Controller
 
     public function getAll(Request $request)
     {
+        //было бы прикольно добавить вывод вместе с юзером последнего сообщения в чате
         $senderUser = $request->user();
-
         $check = $this->checkService->checkUser($request);
-
         if ($check) {
             return response()->json([
                 'status' => 'profile is not approved',
@@ -70,30 +66,28 @@ class MessageController extends Controller
             }
             // Получаем информацию о найденных пользователях
             $users = User::whereIn('id', $userIds)->get();
-
-            return $users;
+            return response()->json([
+                'status' => 'ok',
+                'user_chats' => UserCompactResource::collection($users),
+            ]);
         }
-
     }
 
-    public function store(Request $request,User $user): JsonResponse
+    public function store(Request $request, User $user): JsonResponse
     {
         // Получаем данные формы
         $data = $request->validate([
             'content' => 'required',
         ]);
-
         // Создаем новое сообщение
         $message = new Message([
             'sender_id' => $request->user()->id,
             'receiver_id' => $user->id,
             'content' => $data['content'],
         ]);
-
         // Сохраняем сообщение
         $message->save();
 
         return response()->json(['status' => 'success', 'data' => 'Сообщение отправлено']);
-
     }
 }
