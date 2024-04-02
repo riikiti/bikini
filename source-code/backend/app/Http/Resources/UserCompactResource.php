eac345270427b553ab7c34477317b0b935977c5b
Ruslan <rik.chanel@gmail.com>
2024-04-02 23:49:14 +0300

    add fields

--- source-code/backend/app/Http/Resources/UserCompactResource.php
+++ source-code/backend/app/Http/Resources/UserCompactResource.php
@@ -26,7 +26,7 @@ class UserCompactResource extends JsonResource
             'avatar' => $this->avatar ? $this->appUrl . '/storage/' . $this->avatar : null,
             'country' => CountryResurce::make($this->country),
             'role'=>auth()->user()->role,
-            'fields'=>$this->fields,
+            'info'=>$this->fields,
         ];
         $data['active_contest'] = isset($contest);
         $data['is_favorite'] = isset($favorite);
