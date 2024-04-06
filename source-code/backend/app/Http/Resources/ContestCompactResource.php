95e2761dc0d26fc9ad8e642750c57fc185e8fee7
Ruslan <rik.chanel@gmail.com>
2024-04-06 19:31:31 +0300

    fix contest photo route

--- source-code/backend/app/Http/Resources/ContestCompactResource.php
+++ source-code/backend/app/Http/Resources/ContestCompactResource.php
@@ -12,14 +12,21 @@ class ContestCompactResource extends JsonResource
      *
      * @return array<string, mixed>
      */
+
+    private string $appUrl;
+
     public function toArray(Request $request): array
     {
+        $this->appUrl = config('app.url');
+
         return [
             'name' => $this->name,
             'start' => $this->date_start,
             'finish' => $this->date_finish,
             'active' => $this->is_active,
-            'rating' => $this->rating
+            'rating' => $this->rating,
+            'avatar' => $this->avatar ? $this->appUrl . '/storage/' . $this->avatar : null,
+            'photo' => $this->photo ? $this->appUrl . '/storage/ContestPhotos/' . $this->photo : null,
         ];
     }
 }
