ce21b179fe1998633e2e2e9d75fd3a324908fba7
mlevsha <mlevsha@team.amocrm.com>
2024-04-13 14:19:01 +0300

    fix messenger

--- source-code/backend/app/Http/Resources/WinnerResource.php
+++ source-code/backend/app/Http/Resources/WinnerResource.php
@@ -3,24 +3,18 @@
 namespace App\Http\Resources;
 
 use App\Models\Contest;
-use App\Models\ContestModel;
-use App\Models\Favourite;
-use App\Models\User;
-use App\Models\WinnerList;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class WinnerResource extends JsonResource
 {
-    private string $appUrl;
-
     public function toArray(Request $request): array
     {
-        $contest = ContestModel::query()->where('id', $this->contest_id)->first();
+        $contest = Contest::query()->where('id', $this->contest_id)->first();
 
         return [
             'place' => $this->place,
-            'contest' => $contest,
+            'contest' => $contest->name,
         ];
     }
 
