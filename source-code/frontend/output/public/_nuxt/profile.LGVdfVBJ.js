0c381dcbd9e9becc08229e119bd543fceb95f96a
serjbaidekin <axuitebe13@mail.ru>
2024-04-14 13:13:38 +0300

    build: build s bogom 2

--- source-code/frontend/output/public/_nuxt/profile.LGVdfVBJ.js
+++
@@ -1,1 +0,0 @@
-import{bo as t,a4 as a,bp as s}from"./entry.YpbSQ0aJ.js";import{u}from"./index.aH0vl39t.js";const f=t(e=>{if(e.fullPath.includes(a.PLATFORM_BLOG)||e.fullPath.includes(a.PLATFORM_BOX)||e.fullPath.includes(a.PLATFORM_GALLERY)){const l=u();return s(l(a.PROFILE))}});export{f as default};
