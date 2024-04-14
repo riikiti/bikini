b30cced3e1f638f20bd79330cf85f348dcba1c6b
Ruslan <rik.chanel@gmail.com>
2024-04-14 16:58:38 +0300

    admin searchable

--- source-code/backend/app/Filament/Resources/UserResource/UserResource.php
+++ source-code/backend/app/Filament/Resources/UserResource/UserResource.php
@@ -2,12 +2,10 @@
 
 namespace app\Filament\Resources\UserResource;
 
-use App\Filament\Resources\UserResource\Pages\SendMessageUsers;
 use App\Models\Message;
 use App\Models\User;
 use Filament\Forms\Components\Hidden;
 use Filament\Forms\Components\TextInput;
-use Filament\Forms\Form;
 use Filament\Resources\Resource;
 use Filament\Tables;
 use Filament\Tables\Columns\TextColumn;
@@ -22,11 +20,12 @@ class UserResource extends Resource
     protected static ?string $navigationIcon = 'heroicon-o-user';
 
     protected static ?string $navigationLabel = 'Пользователи';
+
     public static function table(Table $table): Table
     {
         return $table
             ->columns([
-                TextColumn::make('UserId')->label('id')->searchable(),
+                TextColumn::make('id')->label('id')->searchable(),
                 TextColumn::make('email')->label('E-mail')->searchable(),
                 TextColumn::make('created_at')->label('Дата регистрации')->date(),
                 TextColumn::make('role')->label('Роль'),
@@ -52,11 +51,11 @@ class UserResource extends Resource
                         Hidden::make('sender_id')
                             ->label('Id отправителя')
                             ->required()
-                            ->default(fn () => auth()->id()),
+                            ->default(fn() => auth()->id()),
                         Hidden::make('receiver_id')
                             ->label('Id получателя')
                             ->required()
-                            ->default(fn ($record) => $record->UserId),
+                            ->default(fn($record) => $record->UserId),
 
                         // ...
                     ])
@@ -81,7 +80,9 @@ class UserResource extends Resource
             'index' => Pages\ListUsers::route('/'),
         ];
     }
-    public static function getAdminId(Request $request){
+
+    public static function getAdminId(Request $request)
+    {
         return $request->user()->id;
     }
 }
