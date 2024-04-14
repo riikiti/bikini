2f5914b8aa533342f2647f4c9f2becadeeb01607
serjbaidekin <axuitebe13@mail.ru>
2024-04-14 18:56:19 +0300

    build: build app

---
+++ source-code/frontend/.output/public/_nuxt/default.sqTPRnoV.js
@@ -0,0 +1,1 @@
+import{_ as o}from"./_plugin-vue_export-helper.x3n3nnut.js";import{c as t,b3 as s,o as r}from"./entry.cMATa27n.js";const c={},a={class:"h-full"};function n(e,l){return r(),t("div",a,[s(e.$slots,"default")])}const d=o(c,[["render",n]]);export{d as default};
