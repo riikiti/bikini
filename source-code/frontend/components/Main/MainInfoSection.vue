c623eb54a6bf278623c3a683893b73b52b66a7cb
serjbaidekin <axuitebe13@mail.ru>
2024-04-26 00:42:16 +0300

    chore: refactoring app

--- source-code/frontend/components/Main/MainInfoSection.vue
+++ source-code/frontend/components/Main/MainInfoSection.vue
@@ -6,7 +6,10 @@
       class="absolute top-0 left-0 w-full h-full object-cover object-center -z-[5] brightness-75"
     />
     <div class="max-w-[1366px] flex flex-col gap-6 items-center justify-center mx-auto">
-      <img src="/assets/images/login/logo.png" alt="" />
+      <img
+        src="/assets/images/login/logo.png"
+        class="max-w-[300px] w-full sm:max-w-[400px] object-contain"
+      />
       <p class="text-lg sm:text-2xl text-center text-white my-6 max-w-[800px]">
         Найдите участниц из вашего региона. <span class="font-bold">Знакомьтесь</span>, пишите,
         общайтесь
