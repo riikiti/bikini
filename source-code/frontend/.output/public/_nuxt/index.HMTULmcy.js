47930d8f7839ec82ad845afabfa761c539d4bfbc
serjbaidekin <axuitebe13@mail.ru>
2024-04-26 00:36:36 +0300

    chore: refactoring app

--- source-code/frontend/.output/public/_nuxt/index.HMTULmcy.js
+++
@@ -1,1 +0,0 @@
-import{cm as a,cn as o,co as t}from"./entry.UIkGX9-f.js";function e(){return a(o)}function s(){return a(t)}export{s as a,e as u};
---
+++ source-code/frontend/.output/public/_nuxt/profile.UpdCPpBF.js
@@ -0,0 +1,1 @@
+import{bq as t,a2 as a,br as s}from"./entry.ydcKSOmp.js";import{u}from"./index.2h_-J51O.js";const r=t(e=>{if(e.fullPath.includes(a.PLATFORM_BLOG)||e.fullPath.includes(a.PLATFORM_BOX)||e.fullPath.includes(a.PLATFORM_GALLERY)){const l=u();return s(l(a.PROFILE))}});export{r as default};
