a3d47535eea1ae2b6e1853ba3dbb2f1429b8a0bd
serjbaidekin <axuitebe13@mail.ru>
2024-04-27 14:10:18 +0300

    fix(front):add id field for photo. Geniy in blacknot code pishet

--- source-code/backend/app/Http/Resources/ModelPhotoResource.php
+++ source-code/backend/app/Http/Resources/ModelPhotoResource.php
@@ -10,10 +10,11 @@ class ModelPhotoResource extends JsonResource
     private string $appUrl;
 
     public function toArray(Request $request): array
-    {
-        $this->appUrl = config('app.url');
-        return [
-            'photo' => $this->image ? $this->appUrl.'/storage/'.$this->image : null,
-        ];
-    }
+     {
+           $this->appUrl = config('app.url');
+           return [
+               'id' => $this->id,
+               'photo' => $this->image ? $this->appUrl.'/storage/'.$this->image : null,
+           ];
+       }
 }
